1a16be483868b3777f2ae8e66156fb49e70c327b
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-10-28 00:15:38 +0300

    Add files via upload

---
+++ deneme.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// TİP BELİRTEREK DEĞİŞKEN VERMEDE VAR DEGISKENADI INT = DEGER; BELİRTMEDEN VERMEDE VAR DEGISKENADI = DEGER;BELIRTMEDEN TANIMLAMANANIN BİR DİĞER YOLU := İŞARETİ
+	var x = 16
+	var sden string = "Emre"
+	y := 16
+	fmt.Println(sden)
+	fmt.Println(x)
+	fmt.Println(y)
+
+}
